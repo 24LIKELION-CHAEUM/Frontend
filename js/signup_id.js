f73ab229d96990cefd030cf5e764384d295d5443
hyeumm <20221159@sungshin.ac.kr>
2024-07-23 23:47:08 +0900

    delete #1/불필요한 주석 제거

--- js/signup_id.js
+++ js/signup_id.js
@@ -1,22 +1,21 @@
-// 입력 필드와 버튼을 선택합니다.
 const inputId = document.querySelector('.input_id');
 const inputPassword = document.querySelector('.input_password');
 const signinBtn = document.getElementById('signin_btn');
 
-// 입력 필드의 값이 변경될 때마다 호출되는 함수를 정의합니다.
+//값이 변경될 때마다 호출
 function checkInputs() {
     if (inputId.value.trim() && inputPassword.value.trim()) {
-        // 아이디와 비밀번호가 모두 입력되었으면 버튼을 활성화합니다.
+        // 아이디 비밀번호 모두 입력 -> 버튼 활성화
         signinBtn.disabled = false;
     } else {
-        // 아이디와 비밀번호가 모두 입력되지 않았으면 버튼을 비활성화합니다.
+        // 아이디와 비밀번호가 모두 입력되지 않음 -> 버튼 비활성화
         signinBtn.disabled = true;
     }
 }
 
-// 입력 필드에 이벤트 리스너를 추가하여 값이 변경될 때마다 checkInputs 함수를 호출합니다.
+//값이 변경될 때마다 checkInputs 함수 호출
 inputId.addEventListener('input', checkInputs);
 inputPassword.addEventListener('input', checkInputs);
 
-// 페이지 로드 시에도 checkInputs 함수를 호출하여 초기 상태를 설정합니다.
+//페이지 로드 시에도 checkInputs 함수 호출,초기 상태 설정
 window.addEventListener('load', checkInputs);
--- js/signup_name.js
+++ js/signup_name.js
@@ -1,21 +1,19 @@
-// 입력 필드와 버튼을 선택합니다.
 const inputName = document.getElementById('input_name');
 const inputDob = document.getElementById('input_dob');
 const dobError = document.getElementById('dob_error');
 const signinBtn = document.getElementById('signin_btn');
 
-// 입력 필드의 값이 변경될 때마다 호출되는 함수를 정의합니다.
 function checkInputs() {
     const nameValue = inputName.value.trim();
     const dobValue = inputDob.value.trim();
 
-    // 이름 입력값 확인
+    // 이름 확인
     const isNameValid = nameValue.length > 0;
 
-    // 생년월일 입력값 확인
+    // 생년월일 확인
     const isDobValid = dobValue.length === 8 && /^\d{8}$/.test(dobValue);
 
-    // 이름 필드가 비어있지 않고, 생년월일 필드가 유효하면 버튼 활성화
+    // 이름 비어있지 않고, 생년월일 유효 -> 버튼 활성화
     if (isNameValid && isDobValid) {
         signinBtn.disabled = false;
         inputDob.classList.remove('error');
@@ -32,9 +30,7 @@ function checkInputs() {
     }
 }
 
-// 입력 필드에 이벤트 리스너를 추가하여 값이 변경될 때마다 checkInputs 함수를 호출합니다.
 inputName.addEventListener('input', checkInputs);
 inputDob.addEventListener('input', checkInputs);
 
-// 페이지 로드 시에도 checkInputs 함수를 호출하여 초기 상태를 설정합니다.
 window.addEventListener('load', checkInputs);
