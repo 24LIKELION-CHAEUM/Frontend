e66a1521e26e014a030c14292027610a952dd6c3
hyeumm <20221159@sungshin.ac.kr>
2024-07-26 05:29:28 +0900

    mod #18/검색창 이미지 변경 조건 수정

--- js/click.js
+++ js/click.js
@@ -3,18 +3,23 @@ document.addEventListener('DOMContentLoaded', function() {
     const searchIcon = document.querySelector('.search');
 
     if (searchInput && searchIcon) {
-        searchInput.addEventListener('input', function() {
-            if (searchInput.value.trim() !== '') {
+        function updateSearchIcon() {
+            if (searchInput.value.trim() !== '' && !document.activeElement.isEqualNode(searchInput)) {
                 searchIcon.src = '/assets/x.svg'; 
             } else {
                 searchIcon.src = '/assets/search.svg'; 
             }
-        });
+        }
+
+        searchInput.addEventListener('input', updateSearchIcon);
+        searchInput.addEventListener('blur', updateSearchIcon);
+        searchInput.addEventListener('focus', updateSearchIcon);
 
         searchIcon.addEventListener('click', function() {
             if (searchIcon.src.includes('x.svg')) {
                 searchInput.value = ''; 
                 searchInput.dispatchEvent(new Event('input')); 
+                searchInput.focus(); 
             }
         });
     }
@@ -29,5 +34,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const profileNames = document.querySelectorAll('.profile_name');
     profileNames.forEach(name => {
         truncateText(name, 11);
+    });
+
+    var searchButton = document.getElementById("search_button");
+    var dropdown = document.getElementById("dropdown");
+
+    searchButton.addEventListener("click", function() {
+        dropdown.style.display = 'block';
     });
 });
