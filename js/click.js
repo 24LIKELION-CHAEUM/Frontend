9011d3d4926459ad5c731df8d2f6c3498d85c905
hyeumm <20221159@sungshin.ac.kr>
2024-07-26 21:37:05 +0900

    mod #18/콕 찌르기 상태 변경

--- js/click.js
+++ js/click.js
@@ -1,28 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const searchInput = document.querySelector('.input_id');
-    const searchIcon = document.querySelector('.search');
-
-    if (searchInput && searchIcon) {
-        function updateSearchIcon() {
-            if (searchInput.value.trim() !== '' && !document.activeElement.isEqualNode(searchInput)) {
-                searchIcon.src = '/assets/x.svg'; 
-            } else {
-                searchIcon.src = '/assets/search.svg'; 
-            }
-        }
-
-        searchInput.addEventListener('input', updateSearchIcon);
-        searchInput.addEventListener('blur', updateSearchIcon);
-        searchInput.addEventListener('focus', updateSearchIcon);
-
-        searchIcon.addEventListener('click', function() {
-            if (searchIcon.src.includes('x.svg')) {
-                searchInput.value = ''; 
-                searchInput.dispatchEvent(new Event('input')); 
-                searchInput.focus(); 
-            }
-        });
-    }
+    const dropdownMenu = document.getElementById('dropdown-menu');
+    const searchButton = document.getElementById("search-button");
+    const checkmarkButtons = document.querySelectorAll('.profile-checkmark');
+    const checkmarkLabel = document.querySelectorAll('.dropdown-checkmark');
 
     function truncateText(element, maxLength) {
         const text = element.textContent;
@@ -31,15 +11,30 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    const profileNames = document.querySelectorAll('.profile_name');
+    const profileNames = document.querySelectorAll('.profile-name');
     profileNames.forEach(name => {
         truncateText(name, 11);
     });
 
-    var searchButton = document.getElementById("search_button");
-    var dropdown = document.getElementById("dropdown");
-
     searchButton.addEventListener("click", function() {
-        dropdown.style.display = 'block';
+        dropdownMenu.style.display = 'block';
+    });
+
+    checkmarkButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            button.classList.toggle('disabled');
+        });
+    });
+    
+    checkmarkLabel.forEach(button => {
+        button.addEventListener('click', function () {
+            button.classList.toggle('disabled');
+        });
+    });
+
+    document.addEventListener('click', function(event) {
+        if (dropdownMenu.style.display === 'block' && !dropdownMenu.contains(event.target) && !searchButton.contains(event.target)) {
+            dropdownMenu.style.display = 'none';
+        }
     });
 });
